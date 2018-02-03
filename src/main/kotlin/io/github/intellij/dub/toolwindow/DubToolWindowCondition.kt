7fd17c03ed6f292120ee1e52b6e6fdc0eeed0ecb
SingingBush <singingbush@hotmail.com>
2018-02-03 23:55:41 +0000

    fixed DubToolWindowCondition

--- src/main/kotlin/io/github/intellij/dub/toolwindow/DubToolWindowCondition.kt
+++ src/main/kotlin/io/github/intellij/dub/toolwindow/DubToolWindowCondition.kt
@@ -1,11 +1,10 @@
 package io.github.intellij.dub.toolwindow
 
 import com.intellij.openapi.diagnostic.Logger
-import com.intellij.openapi.externalSystem.model.ProjectSystemId
-import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Condition
-import io.github.intellij.dub.Dub
+import com.intellij.openapi.vfs.VirtualFile
+import java.util.*
 
 /**
  * @author Samael Bate (singingbush)
@@ -17,21 +16,12 @@ class DubToolWindowCondition : Condition<Project> {
 
     private val LOG: Logger = Logger.getInstance(DubToolWindowCondition::class.java)
 
+    override fun value(project: Project?): Boolean {
+        LOG.debug("DUB Tool Window Condition: value(${project?.name})")
+        val baseDir = project?.baseDir ?: return false
 
-//    companion object {
-//        val TOOLCHAIN_TOPIC: Topic<ToolchainListener> = Topic(
-//                "toolchain changes",
-//                ToolchainListener::class.java
-//        )
-//    }
-//
-//    interface ToolchainListener {
-//        fun toolchainChanged()
-//    }
-
-    override fun value(t: Project?): Boolean {
-        LOG.debug("DUB Tool Window Condition: value()")
-        val manager = ExternalSystemApiUtil.getManager(Dub.SYSTEM_ID)
-        return true
+        return Arrays.stream<VirtualFile>(baseDir.children)
+            .filter { f -> !f.isDirectory }
+            .anyMatch { f -> "dub.json".equals(f.name, ignoreCase = true) || "dub.sdl".equals(f.name, ignoreCase = true) }
     }
 }
